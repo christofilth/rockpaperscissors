a8dcb242dc3f7c4aeba5715fc89b923dbb26770f
christofilth <171975741+christofilth@users.noreply.github.com>
2024-06-17 15:28:49 +0100

    Write pseudocode for getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+/* Function getComputerChoice 
+    Create variable cpuChoice
+    Generate a random number between 1-100
+    If the number is below 32, change cpuChoice to "Rock"
+    If the number is between 33 and 65, change cpuChoice to "Paper"
+    If the number is between 66 and 100, change cpuChoice to "Scissors"
+    Return cpuChoice
+*/ 
